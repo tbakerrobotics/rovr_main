f47102d3d4a94509a539322329b4239bd9f88357
tbakerrobotics <52080849+tbakerrobotics@users.noreply.github.com>
2021-01-11 09:14:32 +0000

    update

---
+++ Source/rovr_main/lobbyGMParent.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "lobbyGMParent.h"
+#include "Runtime/Core/Public/HAL/ThreadingBase.h"
+
---
+++ Source/rovr_main/lobbyGMParent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "lobbyGMParent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROVR_MAIN_API AlobbyGMParent : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
