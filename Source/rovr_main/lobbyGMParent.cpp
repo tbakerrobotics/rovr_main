de6264b6bf0174c010b80b459250a6d16349d358
tbakerrobotics <52080849+tbakerrobotics@users.noreply.github.com>
2021-01-15 12:02:00 +0000

    mutex implementation

--- Source/rovr_main/lobbyGMParent.cpp
+++ Source/rovr_main/lobbyGMParent.cpp
@@ -4,3 +4,16 @@
 #include "lobbyGMParent.h"
 #include "Runtime/Core/Public/HAL/ThreadingBase.h"
 
+FCriticalSection CriticalSection; // <-- not a pointer, but an actual instance!
+
+void AlobbyGMParent::setUpMutexLock(APlayerController* PC) {
+	// unprotected things here...
+	{
+		// do protected things here...
+
+		FScopeLock Lock(&CriticalSection);
+		Event_MutexPlayerHandler(PC);
+	}
+	// unprotected things here...
+}
+
--- Source/rovr_main/lobbyGMParent.h
+++ Source/rovr_main/lobbyGMParent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameMode.h"
+#include "GameFramework/PlayerController.h"
 #include "lobbyGMParent.generated.h"
 
 /**
@@ -13,5 +14,12 @@ UCLASS()
 class ROVR_MAIN_API AlobbyGMParent : public AGameMode
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void setUpMutexLock(APlayerController* PC);	
 	
+	UFUNCTION(BlueprintImplementableEvent) //BlueprintNativeEvent if you wanted c++ functionality too
+	void Event_MutexPlayerHandler(APlayerController* PC);
 };
+
