eeb19e3aaa1c9d7025b3ef7c6ac355328e4ce4e8
tbakerrobotics <52080849+tbakerrobotics@users.noreply.github.com>
2021-01-15 12:33:52 +0000

    mutex prints

--- Source/rovr_main/lobbyGMParent.cpp
+++ Source/rovr_main/lobbyGMParent.cpp
@@ -2,6 +2,7 @@
 
 
 #include "lobbyGMParent.h"
+#include "Engine/GameEngine.h"
 #include "Runtime/Core/Public/HAL/ThreadingBase.h"
 
 FCriticalSection CriticalSection; // <-- not a pointer, but an actual instance!
@@ -9,10 +10,15 @@ FCriticalSection CriticalSection; // <-- not a pointer, but an actual instance!
 void AlobbyGMParent::setUpMutexLock(APlayerController* PC) {
 	// unprotected things here...
 	{
-		// do protected things here...
+		// do protected things here..
+		if (GEngine)
+			GEngine->AddOnScreenDebugMessage(-1, 30.0f, FColor::Red, TEXT("C SCRIPT - BEFORE LOCK"));
 
 		FScopeLock Lock(&CriticalSection);
 		Event_MutexPlayerHandler(PC);
+
+		if (GEngine)
+			GEngine->AddOnScreenDebugMessage(-1, 30.0f, FColor::Red, TEXT("C SCRIPT - AFTER LOCK"));
 	}
 	// unprotected things here...
 }
